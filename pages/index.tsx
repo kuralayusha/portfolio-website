29d50faa121b99cbcd30770694a2e6becc687056
kurlayusha <tbark.guard@gmail.com>
2023-03-13 01:33:06 +0300

    general fix and clean 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,8 +65,6 @@ export default function Home(props: { pageViwers: number }) {
     })
   }, [r])
 
-  // console.log({ likesData })
-
   useEffect(() => {
     const arr = Object.values(likesData)
     const sum = arr.reduce((a: any, b: any) => a + b, 0)
